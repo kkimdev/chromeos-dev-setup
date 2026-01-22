4c24b8ab11efa2a54d411b566b5b5b3eb4fb2c8c
Kibeom Kim <503414+kkimdev@users.noreply.github.com>
2026-01-22 16:01:23 +0900

    .

--- waydroid/build_kernel.bash
+++ waydroid/build_kernel.bash
@@ -35,8 +35,8 @@ CHROMEOS_KERNEL_FAMILY=termina ./chromeos/scripts/prepareconfig container-vm-x86
 echo "Applying Binder IPC configurations..."
 ./scripts/config --enable CONFIG_ANDROID
 ./scripts/config --enable CONFIG_ANDROID_BINDER_IPC
-./scripts/config --disable CONFIG_ANDROID_BINDERFS  # Disable BinderFS
-./scripts/config --set-str CONFIG_ANDROID_BINDER_DEVICES "binder,hwbinder,vndbinder"
+./scripts/config --enable CONFIG_ANDROID_BINDERFS
+
 # Refresh config to satisfy dependencies
 make LLVM=1 LLVM_IAS=1 olddefconfig
 # --- AUTOMATION END ---
@@ -50,6 +50,7 @@ cp arch/x86/boot/bzImage /mnt/chromeos/MyFiles/Kernels/bzImage
 
 echo "----------------------------------------------------------"
 echo "Kernel moved to MyFiles/Kernels/bzImage"
-echo "Run this in crosh:"
-echo "vmc stop termina && vmc start termina --enable-gpu --kernel /home/chronos/user/MyFiles/Kernels/bzImage"
+echo "Run this in crosh (Ctrl+Alt+T):"
+echo "vmc stop termina"
+echo "vmc start termina --enable-gpu --kernel /home/chronos/user/MyFiles/Kernels/bzImage"
 echo "----------------------------------------------------------"
